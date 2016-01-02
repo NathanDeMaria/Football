840d35171d277166c99f7218f7e59e485a153e61
NathanDeMaria <nathandmaria@gmail.com>
2016-01-02 15:59:05 -0600

    Add some features to the load script

--- science/load_data.py
+++ science/load_data.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 
-def load_data():
+def raw_data():
     """
     Read in the play by play data
     :return: DataFrame of play by play data
@@ -10,3 +10,15 @@ def load_data():
     this_dir = os.path.dirname(os.path.realpath(__file__))
     plays_csv = os.path.join(this_dir, '..', 'plays.csv')
     return pd.read_csv(plays_csv)
+
+
+def load_data():
+    """
+    Loads the data with some computed features
+    :return: DataFrame of play data
+    """
+    data = raw_data()
+    data['offense_win'] = (data.offense_final > data.defense_final).astype(int)
+    data['score_diff'] = data.offense_score - data.defense_score
+    data['score_rate'] = data.score_diff / data.time_left
+    return data
